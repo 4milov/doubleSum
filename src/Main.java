25034ae9d943bc356c915d5eaa0cc972836eadd5
4milo <4milov@gmail.com>
2022-09-03 02:25:36 +0300

    add method sum2 with stream

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(sum(new double[] {1, 5.2, 4, 0, -1}));
@@ -9,5 +12,8 @@ public class Main {
                 sum = sum + number;
             }
             return sum;
+    }
+        public static double sum2 (double[] numbers) {
+            return Arrays.stream(numbers).sum();
     }
 }
